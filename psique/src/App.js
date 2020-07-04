502757e4d91b4f78b3a2f9f50a682737280a88a6
Daniel0205 <daniel.alejandro.diaz@correounivalle.edu.co>
2020-07-04 17:46:38 -0500

    Subtest balanzas-Wais

--- psique/src/App.js
+++ psique/src/App.js
@@ -24,6 +24,8 @@ function App(props) {
         return <Redirect to="/test/wais/cancelacion" />
       case "WAIS-Comprension":
         return <Redirect to="/test/wais/comprension" />
+      case "WAIS-Balanzas":
+        return <Redirect to="/test/wais/balanzas" />
       case "WISC IV":
         return <Redirect to="/test/wisc" />
       case "WISC-selection":
@@ -69,6 +71,7 @@ function App(props) {
       <Route exact path="/test/wais/figurasIncompletas" component={()=><Sidenav body={"WAIS IV"} subtest={"Figuras Incompletas"}></Sidenav>} />{/*Estimulos*/}
       <Route exact path="/test/wais/cancelacion" component={()=><Sidenav body={"WAIS IV"} subtest={"Cancelacion"}></Sidenav>} />{/*Manual*/}
       <Route exact path="/test/wais/comprension" component={()=><Sidenav body={"WAIS IV"} subtest={"Comprension"}></Sidenav>} />{/*Manual*/}
+      <Route exact path="/test/wais/balanzas" component={()=><Sidenav body={"WAIS IV"} subtest={"Balanzas"}></Sidenav>} />{/*Manual*/}
       
       <Route exact path="/test/stroop" component={()=><Sidenav body={"Prueba de STROOP"}></Sidenav>} />
       <Route exact path="/test/king" component={()=><Sidenav body={"Prueba de Rey"}></Sidenav>} />

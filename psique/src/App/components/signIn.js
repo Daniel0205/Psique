3ac526f0c12f63ce09d04600b15a3215af19a25d
Shinku24 <juan.felipe.gil@correounivalle.edu.co>
2020-11-06 17:46:16 -0500

    Changed Login page and added more logo styles

--- psique/src/App/components/signIn.js
+++ psique/src/App/components/signIn.js
@@ -1,15 +1,11 @@
 import React from 'react';
-import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
 import Link from '@material-ui/core/Link';
 import Paper from '@material-ui/core/Paper';
 import Box from '@material-ui/core/Box';
 import Grid from '@material-ui/core/Grid';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import Snackbar from './snackbar'
 import { makeStyles } from '@material-ui/core/styles';
@@ -17,6 +13,12 @@ import { useMutation, gql } from '@apollo/client';
 import { setDoctor } from "../store/doctor/action";
 import { setBody } from "../store/body/action";
 import { connect } from "react-redux";
+
+//import  Logo from "../assets/Logo/logo72x83 - Color.png";
+//import  Logo from "../assets/Logo/logo72x83 - Color Words1.png";
+import  Logo from "../assets/Logo/logo72x83 - Color Words2.png";
+
+import loginBackground from "../assets/Fondo/night-sky.jpg";
 
 const LOGIN_QUERY = gql`
   mutation($username: String!, $password: String!) {
@@ -44,6 +46,10 @@ function Copyright() {
 const useStyles = makeStyles((theme) => ({
   root: {
     height: '100vh',
+    backgroundImage: "url(" + loginBackground + ")",
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
   },
   image: {
     backgroundImage: 'url(https://source.unsplash.com/random)',
@@ -54,7 +60,7 @@ const useStyles = makeStyles((theme) => ({
     backgroundPosition: 'center',
   },
   paper: {
-    margin: theme.spacing(8, 4),
+    margin: theme.spacing(5, 4),
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
@@ -69,6 +75,9 @@ const useStyles = makeStyles((theme) => ({
   },
   submit: {
     margin: theme.spacing(3, 0, 2),
+  },
+  logo: {
+    maxHeight: "130px",
   },
 }));
 
@@ -94,22 +103,23 @@ function SignIn(props) {
   }
 
 
-  return (
-    <Grid container component="main" className={classes.root}>
-       <Snackbar
+  return(
+    <Grid container component="main" className={classes.root} alignItems="center" justify="center">      
+        <Snackbar
           onClose={()=>setMsj('')}
           variant="error" 
-          message={msj}/>
-      <CssBaseline />
-      <Grid item xs={false} sm={4} md={7} className={classes.image} />
-      <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+          message={msj}
+        />
+      <CssBaseline />      
+      
+      <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} >
         <div className={classes.paper}>
-          <Avatar className={classes.avatar}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Login
-          </Typography>
+          <img 
+            className={classes.logo}
+            alt="Logo"
+            src={Logo}
+          />
+          
           <form className={classes.form} noValidate>
             <TextField
               variant="outlined"
@@ -133,10 +143,6 @@ function SignIn(props) {
               onChange={(x)=>setPassword(x.target.value)}
 
             />
-            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember me"
-            />
             <Button
               fullWidth
               variant="contained"
@@ -159,7 +165,7 @@ function SignIn(props) {
             </Box>
           </form>
         </div>
-      </Grid>
+      </Grid>      
     </Grid>
   );
 }

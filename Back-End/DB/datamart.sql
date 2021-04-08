7cd4044f421d3dd6836faae9a36bd4e44257ad02
Daniel <daniel.alejandro.diaz@correounivalle.edu.co>
2021-04-08 10:41:50 -0500

    update data mart sql

--- Back-End/DB/datamart.sql
+++ Back-End/DB/datamart.sql
@@ -60,17 +60,6 @@ INSERT INTO background values(1111,20190520,true,true,true,true,true,true,true,t
 INSERT INTO background values(2222,20190520,true,true,true,true,true,true,true,true,true,
 									false,false,false,false,false,false,false,false,false);
 
-DROP TABLE IF EXISTS crisis CASCADE;
-CREATE TABLE crisis(
-	id_crisis SERIAL PRIMARY KEY,
-	frequency  INT NOT NULL,
-	type VARCHAR(30) NOT NULL
-);
-
-INSERT INTO crisis values(1111,1,'paralisis');
-INSERT INTO crisis values(2222,2,'crisis');
-INSERT INTO crisis values(3333,3,'crisis');
-
 
 DROP TABLE IF EXISTS cognitive_disease CASCADE;
 CREATE TABLE cognitive_disease(
@@ -115,10 +104,15 @@ CREATE TABLE neuropsychological_assessment(
 	id_medication INT NOT NULL,
 	id_date INT NOT NULL,
 	iq INT ,
-	executive_functions INT ,
-	language INT,
-	visuospatial INT,
-	memory INT,
+	verbal_comprehension INT,
+	fluid_reasoning INT,
+	working_memory INT,
+	processing_speed INT,
+	rey_result INT,
+	rey_percentil INT,
+	stroop_word_colour INT,
+	stroop_word INT,
+	stroop_colour INT,
 	FOREIGN KEY(id_background,id_background_date) REFERENCES background(id_background,update_date),
 	FOREIGN KEY(id_demographic,id_demographic_date) REFERENCES demographic(id_demographic,update_date),
 	FOREIGN KEY(id_disease) REFERENCES cognitive_disease(id_disease),
@@ -126,10 +120,89 @@ CREATE TABLE neuropsychological_assessment(
 );
 
 
-INSERT INTO neuropsychological_assessment values(1,1111,20190520,1111,20190520,1111,1111,1111,1,1,1,1,1);
-INSERT INTO neuropsychological_assessment values(2,2222,20190520,2222,20190520,2222,2222,1111,2,2,2,2,2);
-INSERT INTO neuropsychological_assessment values(3,3333,20190520,3333,20190520,3333,3333,1111,3,3,3,3,3);
-INSERT INTO neuropsychological_assessment values(4,1111,20190520,1111,20190520,3333,3333,2222,3,3,3,3,3);
-INSERT INTO neuropsychological_assessment values(5,1111,20190520,1111,20190520,2222,3333,2222,3,3,3,3,3);
-INSERT INTO neuropsychological_assessment values(6,2222,20190520,2222,20190520,2222,3333,3333,3,3,3,3,3);
-INSERT INTO neuropsychological_assessment values(7,3333,20190520,2222,20190520,2222,3333,3333,3,3,3,3,3);
+INSERT INTO neuropsychological_assessment values(1,1111,20190520,1111,20190520,1111,1111,1111,1,1,1,1,1,1,1,1,1);
+INSERT INTO neuropsychological_assessment values(2,2222,20190520,2222,20190520,2222,2222,1111,2,2,2,2,2,2,2,2,2);
+INSERT INTO neuropsychological_assessment values(3,3333,20190520,3333,20190520,3333,3333,1111,3,3,3,3,null,3,3,3,3);
+INSERT INTO neuropsychological_assessment values(4,1111,20190520,1111,20190520,3333,3333,2222,3,3,3,3,3,3,3,3,3);
+INSERT INTO neuropsychological_assessment values(5,1111,20190520,1111,20190520,2222,3333,2222,3,3,null,null,null,null,3,3,3);
+INSERT INTO neuropsychological_assessment values(6,2222,20190520,2222,20190520,2222,3333,3333,3,3,3,3,3,3,3,3,3);
+INSERT INTO neuropsychological_assessment values(7,3333,20190520,2222,20190520,2222,3333,3333,3,3,3,3,3,null,3,null,3);
+
+
+/* WADAAAAAAAAAAAAAAAAAAAAAAAAAA */
+
+
+DROP TABLE IF EXISTS stage CASCADE;
+CREATE TABLE stage(
+	id_stage INT PRIMARY KEY,
+	stage TEXT NOT NULL
+);
+
+INSERT INTO stage values(1111,'hemisferio derecho cedado');
+INSERT INTO stage values(2222,'hemisferio Izquierdo cedado');
+INSERT INTO stage values(3333,'Ningun hemisferio cedado');
+
+
+DROP TABLE IF EXISTS aphasia CASCADE;
+CREATE TABLE aphasia(
+	id_aphasia INT PRIMARY KEY,
+	aphasia TEXT NOT NULL,
+	time  INT NOT NULL
+);
+
+INSERT INTO aphasia values(1111,'Aphasia 1',15);
+INSERT INTO aphasia values(2222,'Apahsia 2',20);
+INSERT INTO aphasia values(3333,'Aphasia 3',25);
+
+
+DROP TABLE IF EXISTS motor_deficit CASCADE;
+CREATE TABLE motor_deficit(
+	id_motor_deficit INT PRIMARY KEY,
+	deficit TEXT NOT NULL
+);
+
+
+INSERT INTO motor_deficit values(1111,'Deficit 1');
+INSERT INTO motor_deficit values(2222,'Deficit 2');
+INSERT INTO motor_deficit values(3333,'Deficit 3');
+
+DROP TABLE IF EXISTS chirurgical_assessment CASCADE;
+CREATE TABLE chirurgical_assessment(
+	id_assessment INT PRIMARY KEY,
+	id_background INT NOT NULL,
+	id_background_date INT NOT NULL,
+	id_demographic INT NOT NULL,
+	id_demographic_date INT NOT NULL,
+	id_disease INT NOT NULL,
+	id_medication INT NOT NULL,
+	id_date INT NOT NULL,
+
+	id_stage INT NOT NULL,
+	id_aphasia INT NOT NULL,
+	id_motor_deficit INT NOT NULL,
+	
+	counting INT ,
+	denomination INT,
+	verbal_instructions INT,
+	repetition INT,
+	processing_speed INT,
+	lecture INT,
+	follow_instructions INT,
+
+	FOREIGN KEY(id_background,id_background_date) REFERENCES background(id_background,update_date),
+	FOREIGN KEY(id_demographic,id_demographic_date) REFERENCES demographic(id_demographic,update_date),
+	FOREIGN KEY(id_disease) REFERENCES cognitive_disease(id_disease),
+	FOREIGN KEY(id_medication) REFERENCES medication(id_medication),
+	FOREIGN KEY(id_stage) REFERENCES stage(id_stage),
+	FOREIGN KEY(id_aphasia) REFERENCES aphasia(id_aphasia),
+	FOREIGN KEY(id_motor_deficit) REFERENCES motor_deficit(id_motor_deficit)
+);
+
+
+INSERT INTO chirurgical_assessment values(1,1111,20190520,1111,20190520,1111,1111,1111,1111,1111,1111,null,null,1,2,1,1);
+INSERT INTO chirurgical_assessment values(2,2222,20190520,2222,20190520,2222,2222,1111,1111,1111,1111,null,null,1,2,1,1);
+INSERT INTO chirurgical_assessment values(3,3333,20190520,3333,20190520,3333,3333,1111,1111,1111,1111,null,null,1,2,1,1);
+INSERT INTO chirurgical_assessment values(4,1111,20190520,1111,20190520,3333,3333,2222,1111,1111,1111,null,null,1,2,1,1);
+INSERT INTO chirurgical_assessment values(5,1111,20190520,1111,20190520,2222,3333,2222,1111,1111,1111,null,null,1,2,1,1);
+INSERT INTO chirurgical_assessment values(6,2222,20190520,2222,20190520,2222,3333,3333,1111,1111,1111,null,null,1,2,1,1);
+INSERT INTO chirurgical_assessment values(7,3333,20190520,2222,20190520,2222,3333,3333,1111,1111,1111,null,null,1,2,1,1);

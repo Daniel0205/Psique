83f6b94a30d0c136c2f2a141ed981ea90d751cb3
stebanca1998 <steban.cadena@correounivalle.edu.co>
2021-04-24 13:43:47 -0500

    Init consult patient

--- psique/src/App/body/patientModule.js
+++ psique/src/App/body/patientModule.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+import MaterialTable from 'material-table';
 
+/*
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
-/* 
+
 import Paper from '@material-ui/core/Paper';
 import InputBase from '@material-ui/core/InputBase';
 import IconButton from '@material-ui/core/IconButton';
@@ -59,12 +61,8 @@ const useStyles = makeStyles((theme) => ({
 
 function PatientModule(props) {
 
-    const classes = useStyles();
-    
-    return(
-    
-    <div  className={classes.bodypage} >  
-        <div className={classes.grid}>
+    /* COMENTARIO DE EL CAR BUTTON CREAR PACIENTE EN CASO DE SER NECESARIO
+    <div className={classes.grid}>
           
             <div className={classes.cardButton}>
                 <Card className={classes.root}>
@@ -79,6 +77,94 @@ function PatientModule(props) {
             </div>
 
         </div>
+    */
+
+    const classes = useStyles();
+/*
+    {
+      id_patient:ID!,	
+      name:String!,
+      surname: String!,
+      gender:String!,
+      city:String!,
+      birth_date: Date!
+    }
+*/
+    const [state, setState] = React.useState({
+      columns: [
+        { title: 'Identificador', field: 'id_patient' },
+        { title: 'Nombres', field: 'name' },
+        { title: 'Apellidos', field: 'surname' },
+        { title: 'Genero', field: 'gender' },
+        { title: 'Ciudad', field: 'city' },
+      ],
+      data: [
+        {
+          id_patient:1234194,	
+          name:'Pepito',
+          surname: 'Perez',
+          gender:'M',
+          city:'Cali',
+          birth_date: 'x-y-z'
+        },
+        {
+          id_patient:1234236,	
+          name:'Pedrito',
+          surname: 'Pereira',
+          gender:'M',
+          city:'Cali',
+          birth_date: 'x-y-z'
+        },
+      ],
+    });
+    
+    return(
+    
+    <div  className={classes.bodypage} >
+
+      <MaterialTable
+        title="Editable Example"
+        columns={state.columns}
+        data={state.data}
+        editable={{
+          onRowAdd: newData =>
+            new Promise(resolve => {
+              setTimeout(() => {
+                resolve();
+                setState(prevState => {
+                  const data = [...prevState.data];
+                  data.push(newData);
+                  return { ...prevState, data };
+                });
+              }, 600);
+            }),
+          onRowUpdate: (newData, oldData) =>
+            new Promise(resolve => {
+              setTimeout(() => {
+                resolve();
+                if (oldData) {
+                  setState(prevState => {
+                    const data = [...prevState.data];
+                    data[data.indexOf(oldData)] = newData;
+                    return { ...prevState, data };
+                  });
+                }
+              }, 600);
+            }),
+          onRowDelete: oldData =>
+            new Promise(resolve => {
+              setTimeout(() => {
+                resolve();
+                setState(prevState => {
+                  const data = [...prevState.data];
+                  data.splice(data.indexOf(oldData), 1);
+                  return { ...prevState, data };
+                });
+              }, 600);
+            }),
+        }}
+      />  
+        
     </div>
     )
 

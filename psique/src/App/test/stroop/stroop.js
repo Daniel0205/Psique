3f801be432f950cc12b3447478d54dd69123dffe
Shinku24 <juan.felipe.gil@correounivalle.edu.co>
2021-09-13 23:16:26 -0500

    small stroop change

--- psique/src/App/test/stroop/stroop.js
+++ psique/src/App/test/stroop/stroop.js
@@ -3,7 +3,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import TestStart from '../../components/testStart';
 import CustomButton from '../../components/customButton';
 import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper';
+//import Paper from '@material-ui/core/Paper';
 import Button from '@material-ui/core/Button';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
